1e0656384372bc78d5377f424ffaf763de8e83e6
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-06-23 14:03:57 +0200

    Event Warnings being shown at the Index page

--- src/DAFA.Presentation.UI.MVC/Controllers/HomeController.cs
+++ src/DAFA.Presentation.UI.MVC/Controllers/HomeController.cs
@@ -1,12 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using DAFA.Application.Interfaces;
+using DAFA.Application.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace DAFA.Presentation.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEventWarningAppService eventWarningAppService;
+
+        public HomeController(IEventWarningAppService eventWarningAppService)
+        {
+            this.eventWarningAppService = eventWarningAppService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var loggedId = User.Identity.GetUserId();
+
+            if (loggedId == null || !Guid.TryParse(loggedId, out Guid userId))
+            {
+                return View(new List<EventWarningViewModel>());
+            }
+
+            var eventWarnings = eventWarningAppService.GetUnsolved();
+            return View(eventWarnings);
         }
 
         public ActionResult About()
