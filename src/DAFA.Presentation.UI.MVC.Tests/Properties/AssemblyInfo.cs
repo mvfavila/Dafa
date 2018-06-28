82c93411715c8a08b87406e46aa7663982088fa4
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-06-28 23:07:19 +0200

    DAFA.Presentation.UI.MVC.Tests project added. Mainly for JS tests

---
+++ src/DAFA.Presentation.UI.MVC.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DAFA.Presentation.UI.MVC.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DAFA.Presentation.UI.MVC.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0d56610-9d9d-47e7-a2db-bfe8ced9c12e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/DAFA.Presentation.UI.MVC.Tests/Scripts/Custom/Field/EditTests.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="../../../../dafa.presentation.ui.mvc/scripts/knockout-3.4.2.js" />
+/// <reference path="../../../../dafa.presentation.ui.mvc/scripts/custom/field/edit.js" />
+
+describe('addNewEvent', function () {
+    var fieldId, eventsList;
+
+    // Arrange
+    beforeEach(function () {
+        fieldId = 'f979e123-6f11-4bac-9c41-8d534afc640a';
+        eventsList = ko.observableArray([]);
+    });
+
+    it('New event must be added to list', function () {
+        // Act
+        var initialListLength = eventsList().length;
+        addNewEvent(fieldId, eventsList);
+        var newListLength = eventsList().length;
+        // Assert
+        expect(newListLength).toBe(initialListLength + 1);
+    });
+
+    it('Correct event must be added to list', function () {
+        // Act
+        addNewEvent(fieldId, eventsList);
+        var unwrappedList = eventsList();
+        var expectedNewEntry = { EventId: fieldId };
+        var selectedEvents = unwrappedList.filter(function (item) {
+            return item.FieldId === fieldId;
+        });
+        var selectedEventsLength = selectedEvents.length;
+        // Assert
+        expect(selectedEventsLength).toBe(1);
+    });
+
+    //it('Event must be added to the end of the list', function () {
+    //    // Act
+    //    addNewEvent(fieldId, eventsList);
+    //    var unwrappedList = eventsList();
+    //    var lastEntry = unwrappedList[unwrappedList.length - 1];
+    //    // Assert
+    //    expect(lastEntry.FieldId).toBe(fieldId);
+    //});
+});
