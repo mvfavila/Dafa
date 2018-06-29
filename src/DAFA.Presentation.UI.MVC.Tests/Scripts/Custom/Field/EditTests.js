42396caf7699a9f3929a737a88ee7c2fa08b8958
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-06-29 16:06:13 +0200

    Tests added for addNewEventObj()

--- src/DAFA.Presentation.UI.MVC.Tests/Scripts/Custom/Field/EditTests.js
+++ src/DAFA.Presentation.UI.MVC.Tests/Scripts/Custom/Field/EditTests.js
@@ -41,3 +41,52 @@ describe('addNewEvent', function () {
         expect(lastEntry.FieldId).toBe(fieldId);
     });
 });
+
+describe('addNewEventObj', function () {
+    var eventId, name, description, date, type, fieldId, eventsList;
+
+    // Arrange
+    beforeEach(function () {
+        eventId = '46bcc34f-e20f-4448-bcbf-7ec0aa142b4a';
+        name = 'DNPM Licence';
+        description = 'Licence required by the DNPM';
+        date = '29/06/2018';
+        type = 'b019838e-c20f-4cc2-9276-deaa209f7434';
+        fieldId = 'f979e123-6f11-4bac-9c41-8d534afc640a';
+        eventsList = ko.observableArray([]);
+    });
+
+    it('New event must be added to list', function () {
+        // Act
+        var initialListLength = eventsList().length;
+        addNewEventObj(eventId, name, description, date, type, fieldId, eventsList);
+        var newListLength = eventsList().length;
+        // Assert
+        expect(newListLength).toBe(initialListLength + 1);
+    });
+
+    it('Correct event must be added to list', function () {
+        // Act
+        addNewEventObj(eventId, name, description, date, type, fieldId, eventsList);
+        var unwrappedList = eventsList();
+        var expectedNewEntry = {
+            EventId: eventId,
+            Name: name,
+            Description: description,
+            Date: date,
+            EventTypeId: type,
+            FieldId: fieldId            
+        };
+        // Assert
+        expect(unwrappedList).toContain(expectedNewEntry);
+    });
+
+    it('Event must be added to the end of the list', function () {
+        // Act
+        addNewEventObj(eventId, name, description, date, type, fieldId, eventsList);
+        var unwrappedList = eventsList();
+        var lastEntry = unwrappedList[unwrappedList.length - 1];
+        // Assert
+        expect(lastEntry.EventId).toBe(eventId);
+    });
+});
